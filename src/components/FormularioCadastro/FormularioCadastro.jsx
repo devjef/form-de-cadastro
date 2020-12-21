832aadc89b5723207a3475d499c6b3810ae237cb
Jeferson <jeferson.ol.lopes@gmail.com>
2020-12-21 15:10:55 -0300

    mudança dinamica de formulário

--- src/components/FormularioCadastro/FormularioCadastro.jsx
+++ src/components/FormularioCadastro/FormularioCadastro.jsx
@@ -2,14 +2,29 @@ import React, { useState } from "react";
 import DadosPessoais from './DadosPessoais';
 import DadosUsuario from './DadosUsuario';
 import DadosEntrega from './DadosEntrega';
+import { Typography } from '@material-ui/core';
 
 function FormularioCadastro({ aoEnviar, validaCPF }) {
 
+  const [etapaAtual, setEtapaAtual] = useState(3);
+
+  function FormularioAtual(etapa) {
+    switch (etapa) {
+      case 0:
+        return <DadosPessoais aoEnviar={aoEnviar} validaCPF={validaCPF} />
+      case 1:
+        return <DadosUsuario />
+      case 2:
+        return <DadosEntrega />
+      default:
+        return <Typography>Erro ao carregar Formulário</Typography>
+    }
+
+  }
+
   return (
     <>
-      <DadosPessoais aoEnviar={aoEnviar} validaCPF={validaCPF} />
-      <DadosUsuario />
-      <DadosEntrega />
+      {FormularioAtual(etapaAtual)}
     </>
   );
 }
